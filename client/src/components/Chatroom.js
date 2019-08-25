08cb3abb85c86f6357f6703633aa828efcfce206
PooperScooper <roberts.blums1@gmail.com>
2019-08-25 15:47:59 +0300

    refactor message send input

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
+import styled from 'styled-components'
+
+const InputPanel = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  align-self: center;
+  border-top: 1px solid #fafafa;
+`
 
 export class Chatroom extends Component {
   state = {
-    message: '',
+    input: '',
     chatHistory: []
   }
   
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+  onInput = (e) => this.setState({ input: e.target.value })
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.sendMessage(this.state.message);
-    this.setState({ message: '' });
+  onSendMessage = (e) => {
+    if (!this.state.input)
+      return
+
+    this.props.sendMessage(this.state.input);
+    this.setState({ input: '' });
   }
 
   componentDidMount = () => {
@@ -29,23 +43,21 @@ export class Chatroom extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
-          <input
-            type='text'
-            name='message'
-            placeholder='Say something nice...'
-            style={{ flex: '10', padding: '5px' }}
-            value={this.state.message}
-            onChange={this.onChange}
+        <InputPanel>
+          <TextField
+            placeholder="Type a message"
+            value={this.state.input}
+            onChange={this.onInput}
+            onKeyPress={e => (e.key === 'Enter' ? this.onSendMessage() : null)}
           />
-          <input
-            type='submit'
-            value='Submit'
-            className='btn'
-            style={{ flex: '1' }}
-          />
-        </form>
-
+          <Fab
+            size="small"
+            onClick={this.onSendMessage}
+            style={{ marginLeft: 20 }}
+          >
+            <SendIcon />
+          </Fab>
+        </InputPanel>
         {this.state.chatHistory.map(msg => <p>{msg}</p>)}
       </div>
     )
